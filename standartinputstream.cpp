afebbd6b64685d48de0392f2b7f28a3acc4297ac
Hakanozs <hknozsvn@gmail.com>
2025-08-30 23:27:22 +0300

    first commit

---
+++ standartinputstream.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ {
+    int age;
+    cin >> age;
+
+    cout << "your age " << age << "\n";
+
+    cin.get(); // Waits for user input to close the program
+    cin.get();
+    return 0;
+}
